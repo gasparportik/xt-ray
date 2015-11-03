576a69442097edb8cd9fce89eec5d0fa5d124d28
boykathemad <madboyka@yahoo.com>
2015-11-03 19:34:48 +0200

    Console: added argument for dump style, overall usability improvements

--- Xtv.Console/Program.cs
+++ Xtv.Console/Program.cs
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+using System;
+using System.Diagnostics;
+using Cout = System.Console;
+
 namespace Xtv.Console
 {
     using Parser;
@@ -10,31 +14,63 @@ namespace Xtv.Console
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            if (args.Length == 0)
             {
-                System.Console.WriteLine("Processing:" + args[0]);
-                var parser = Parser.ParseFile(args[0]);
-                System.Console.WriteLine(parser.GetInfo());
-                System.Console.WriteLine();
-                dump(new Trace[] { parser.RootTrace });
-                
+                Cout.WriteLine(string.Format("Usage: {0} <filename> [-n]", Process.GetCurrentProcess().MainModule.FileName));
+                Cout.WriteLine("The dump style arguments (n) are as follows: ");
+                var index = 0;
+                foreach (var style in Enum.GetNames(typeof(Trace.DumpStyle))) {
+                    Cout.WriteLine($" -{index++} : {style}");
+                }
+                return;
             }
-            else
+            try
             {
-                System.Console.WriteLine("No input file given.");
+                var options = ParseArgs(args);
+                Cout.WriteLine("Processing:" + options.Item1);
+                var parser = Parser.ParseFile(options.Item1);
+                Cout.WriteLine(parser.GetInfo());
+                Cout.WriteLine();
+                Dump(new Trace[] { parser.RootTrace }, options.Item2);
+            }
+            catch (Exception ex)
+            {
+                Cout.WriteLine("Error: " + ex.Message);
             }
         }
 
-        static void dump(Trace[] traces)
+        private static void Dump(Trace[] traces, int style)
         {
-            foreach(var trace in traces)
+            foreach (var trace in traces)
             {
-                System.Console.WriteLine(trace);
+                Cout.WriteLine(trace.ToString(style));
                 if (trace.Children.Length > 0)
                 {
-                    dump(trace.Children);
+                    Dump(trace.Children, style);
                 }
             }
+        }
+
+        private static Tuple<string, int> ParseArgs(string[] args)
+        {
+            string file = null;
+            int style = (int)Trace.DumpStyle.HumanReadable;
+            foreach (var arg in args)
+            {
+                if (arg[0] == '-')
+                {
+                    style = int.Parse(arg.Substring(1));
+                }
+                else
+                {
+                    file = arg;
+                }
+            }
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                throw new ArgumentException("You have to specify a trace file as input!");
+            }
+            return new Tuple<string, int>(file, style);
         }
     }
 }
