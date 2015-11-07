b37f6dd8b25f57b2f0a61934bbb3b26d674fec9f
boykathemad <madboyka@yahoo.com>
2015-11-07 22:37:27 +0200

    Added Gtk# gui, which is very similar to the Windows version, some features are missing

---
+++ Xtv.GtkSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xdebug Trace Viewer")]
+[assembly: AssemblyDescription("Displays xdebug trace files (.xt) on a GUI")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BoykaTheMad")]
+[assembly: AssemblyProduct("Xtv.GtkSharp.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2015 BoykaTheMad")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
