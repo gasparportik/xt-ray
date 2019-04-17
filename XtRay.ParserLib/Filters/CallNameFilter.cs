e230f4fc581f9f44fa25c16329da4dd0942ff945
Jerry Yan <792602257@qq.com>
2019-04-17 20:02:38 +0800

    Param filter support ( will provide a method to use this )

--- XtRay.ParserLib/Filters/CallNameFilter.cs
+++ XtRay.ParserLib/Filters/CallNameFilter.cs
@@ -6,8 +6,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
- using System.Text.RegularExpressions;
- using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace XtRay.Common.Filters
 {
